c212720529c83bf04d066c609594ba3b5fc9604e
TinhHuynh <tinhhuynh.dev@gmail.com>
2022-09-30 16:53:29 +0700

    Remove irrelevant text from footer HTML

--- lib/genius_lyrics.dart
+++ lib/genius_lyrics.dart
@@ -226,9 +226,14 @@ class Genius {
     String getResponse = (await http.get(Uri.parse(Uri.encodeFull(url)))).body;
 
     BeautifulSoup bs = BeautifulSoup(getResponse.replaceAll('<br/>', '\n'));
-    String excluded = bs.find("div", class_: "Lyrics__Footer")?.getText().trim()?? '';
+
+    String excludedFooter =
+        bs.find("div", class_: "Lyrics__Footer")?.getText().trim() ?? '';
+
     return (bs.find("div", class_: "Lyrics__Root")?.getText().trim() ??
-        bs.find("div", class_: "lyrics")?.getText().trim())?.replaceAll(excluded, '');
+        bs.find("div", class_: "lyrics")?.getText().trim())
+        ?.replaceAll(RegExp('You might also like[0-9]+Embed'), '')
+        .replaceAll(excludedFooter, '');
   }
 
   /// Searches for a specific song and gets its lyrics returning [Song] in case it's successful and `null` otherwise .
