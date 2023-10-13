52d29487d552bbc28a0a3865e1d08a5cec31b71e
hbtalha <helderbatalhadvlpr@gmail.com>
2023-10-13 20:47:28 -0100

    fix missing enclosing curly braces

--- lib/models/artist.dart
+++ lib/models/artist.dart
@@ -65,7 +65,8 @@ class Artist {
     }
     _socialNetwork = SocialNetwork.fromJson(artistInfo);
     _about = artistInfo['description']?['plain'];
-
+  }
+  
   /// Returns song data and this data have some fields that are not present in the [Artist]
   Map<String, dynamic> get toJson => _artistInfo;
 
